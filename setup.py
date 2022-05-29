097ee0126171cd6e66c2c1e192f06c0ed3c4b342
Andrew Stewart <andrew.c.stewart@gmail.com>
2022-05-29 09:31:38 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="files-gitpod",
+    version="0.1",
+    description="Meltano project files for gitpod",
+    packages=find_packages(),
+    package_data={
+        "bundle": [
+            ".gitpod.yml",
+            ".gitpod/Dockerfile",
+        ]
+    },
+)
